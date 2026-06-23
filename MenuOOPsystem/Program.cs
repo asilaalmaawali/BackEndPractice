f1412639ae23f2d3a05c1491d02693481de86283
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-23 10:56:38 +0400

    Adding menu and while loop and cases

--- MenuOOPsystem/Program.cs
+++ MenuOOPsystem/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MenuOOPsystem
+﻿using Microsoft.Win32;
+
+namespace MenuOOPsystem
 {
     internal class Program
     {
@@ -16,24 +18,68 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("========================================");
+                Console.WriteLine("   Flight Management System   ");
+                Console.WriteLine("========================================");
+                Console.WriteLine(" 1. Register a Passenger");
+                Console.WriteLine(" 2. Add an Aircraft");
+                Console.WriteLine(" 3. Register a Pilot");
+                Console.WriteLine(" 4. View All Flights");
+                Console.WriteLine(" 5. Schedule a Flight");
+                Console.WriteLine(" 6. Book a Flight");
+                Console.WriteLine(" 7. Cancel a Booking");
+                Console.WriteLine(" 8. Depart a Flight");
+                Console.WriteLine(" 0. Exit");
+                Console.WriteLine("========================================");
+                Console.Write("Select option: ");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                         break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4: 
+                        break;
+                    case 5: 
+                        break;
+                    case 6: 
+                        break;
+                    case 7: 
+                        break;
+                    case 8:
+                        break;
+                    case 9: 
+                        break;
+                    case 10: 
+                        break;
+                    case 0:
+                        exit = true; break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+
+                if (!exit)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
         }
     }
 }

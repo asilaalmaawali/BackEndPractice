bc8b0c73ca1b5fae4932fe39bd32dab9b99ae4e4
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-30 12:18:22 +0400

    Added property for Enrollment class

--- University System Models/Models/Enrollment.cs
+++ University System Models/Models/Enrollment.cs
@@ -6,5 +6,12 @@ namespace University_System_Models.Models
 {
     internal class Enrollment
     {
+        public int EnrollmentId { get; set; }  // system generated
+        public int StudentId { get; set; }  //from list // foreign key from Student class
+        public int CourseId { get; set; }  //from list // foreign key from Course class
+        public DateTime EnrollmentDate { get; set; } // System generated (because we will use (date.now)method)
+
+        public string FinalGrade { get; set; } // user input
+        public string Status { get; set; } // as default "In Progress"
     }
 }

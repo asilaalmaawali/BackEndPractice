90249f276616f3e646177bfe3a714633feb20c78
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-30 15:47:44 +0400

    Added DataAnnotations for Enrollment class

--- University System Models/Models/Enrollment.cs
+++ University System Models/Models/Enrollment.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace University_System_Models.Models
 {
     internal class Enrollment
     {
+        [Key]
+        [Required]
         public int EnrollmentId { get; set; }  // system generated
+        [ForeignKey("Student")]
+        [Required]
         public int StudentId { get; set; }  //from list // foreign key from Student class
+        [ForeignKey("Course")]
+        [Required]
         public int CourseId { get; set; }  //from list // foreign key from Course class
+        [Required]
         public DateTime EnrollmentDate { get; set; } // System generated (because we will use (date.now)method)
-
-        public string FinalGrade { get; set; } // user input
-        public string Status { get; set; } // as default "In Progress"
+        [MaxLength(2)]
+        public string? FinalGrade { get; set; } // user input  // can be null 
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; } = "In Progress";// as default "In Progress"
     }
 }

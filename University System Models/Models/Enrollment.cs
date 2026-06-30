9d1094c4c56282685ce8391f64e80bbfddcb3416
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-30 12:01:07 +0400

    Added models folder and classes and property of student

---
+++ University System Models/Models/Enrollment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University_System_Models.Models
+{
+    internal class Enrollment
+    {
+    }
+}
---
+++ University System Models/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University_System_Models.Models
+{
+    internal class Student
+    {
+
+        public int StudentId { get; set; }  // system generated
+        public string fullName { get; set; }  // user input
+        public string email { get; set; }  // user input
+        public string phoneNumber { get; set; }  // user input
+        public DateTime dateOfBirth { get; set; }  // Calculated 
+        public int enrollmentYear { get; set; }  // user input
+        public decimal gpa { get; set; }  //as default = 0
+
+
+
+    }
+}

e3b569b60e058984fd72764a6ff58bc668cfad18
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-06-30 12:14:13 +0400

    Added property for Course class

--- University System Models/Models/Course.cs
+++ University System Models/Models/Course.cs
@@ -6,5 +6,14 @@ namespace University_System_Models.Models
 {
     internal class Course
     {
+        public int CourseId { get; set; }  // system generated
+        public string CourseCode { get; set; }  // system generated
+        public string CourseTitle { get; set; }  // user input
+
+        public int CreditHours { get; set; }  // user input
+        public int DepartmentId { get; set; }  //from list // foreign key from Department class
+        public int InstructorId { get; set; }   //from list // foreign key from Instructor class
+        public string SemesterOffered { get; set; }  // user input
+
     }
 }

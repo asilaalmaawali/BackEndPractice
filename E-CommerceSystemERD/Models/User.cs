ba4788caee06cebc7bb466e248df8dc700d8bd3e
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 11:51:44 +0400

    Added user class property

--- E-CommerceSystemERD/Models/User.cs
+++ E-CommerceSystemERD/Models/User.cs
@@ -6,5 +6,15 @@ namespace E_CommerceSystemERD.Models
 {
     internal class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }

addab6e984ab0b0851109c1d965f444288d523df
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 11:58:09 +0400

    Added Product class property

--- E-CommerceSystemERD/Models/Product.cs
+++ E-CommerceSystemERD/Models/Product.cs
@@ -6,5 +6,16 @@ namespace E_CommerceSystemERD.Models
 {
     internal class Product
     {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime createdAt { get; set; }  
+        public bool isAvailable { get; set; } 
+
     }
 }

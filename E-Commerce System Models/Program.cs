19cbe7625027ca91b910dc90c2890a6441bbdeed
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 11:18:53 +0400

    Added project

---
+++ E-Commerce System Models/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce_System_Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

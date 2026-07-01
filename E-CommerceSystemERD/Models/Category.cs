2f13c9642f30f156b2dffe45227c1b06bffc5ff9
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 11:53:31 +0400

    Added category class property

--- E-CommerceSystemERD/Models/Category.cs
+++ E-CommerceSystemERD/Models/Category.cs
@@ -6,5 +6,11 @@ namespace E_CommerceSystemERD.Models
 {
     internal class Category
     {
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
     }
 }

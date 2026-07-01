3ee6c84d7d459bf86ccde928aba774c37dfc16a2
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 12:00:23 +0400

    Added Order class property

--- E-CommerceSystemERD/Models/Order.cs
+++ E-CommerceSystemERD/Models/Order.cs
@@ -6,5 +6,13 @@ namespace E_CommerceSystemERD.Models
 {
     internal class Order
     {
+
+        public int OrderId  { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public string ShippingAddress { get; set; }
+        public string PaymentMethod { get; set; }
     }
 }

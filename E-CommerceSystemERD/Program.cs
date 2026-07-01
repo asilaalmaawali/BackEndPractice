8f2d9f2f417ae5cd0b2d89d55afe4f35cd9c854c
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-01 11:43:17 +0400

    Added E-CommerceSystemERD project

---
+++ E-CommerceSystemERD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_CommerceSystemERD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

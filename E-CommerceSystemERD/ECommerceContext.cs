f30f6ce0320d7f00e751fe694e735bc414dad70e
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-06 16:30:48 +0400

    adding ECommerceContext.cs and inherits from DbContext and register all models

---
+++ E-CommerceSystemERD/ECommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using E_CommerceSystemERD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_CommerceSystemERD
+{
+    internal class ECommerceContext : DbContext  // to inherits from DbContext to connect the models with the database
+    {
+        // Registers and manages all model records in the database
+        public DbSet<Category> Categories { get; set; }  // Register , Stores and manages Category records
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+        public DbSet<Review> reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}

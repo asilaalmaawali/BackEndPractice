9c39f4e32928a9d0c5ed02fa486e34d09e5916c9
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-07 17:56:33 +0400

    Adding the cass and menu

--- E-CommerceSystemERD/Program.cs
+++ E-CommerceSystemERD/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("========================================");
+                Console.WriteLine("   E-Commerce System   ");
+                Console.WriteLine("========================================");
+                Console.WriteLine(" 1.  Register a New User");
+                Console.WriteLine(" 2.  Add a New Product to a Category");
+                Console.WriteLine(" 3.  Place an Order");
+                Console.WriteLine(" 4.  Write a Product Review");
+                Console.WriteLine(" 5.  Update Product Price and Availability");
+                Console.WriteLine(" 6.  Cancel an Order");
+                Console.WriteLine(" 0. Exit");
+                Console.WriteLine("========================================");
+                Console.Write("Select option: ");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 5:
+                       
+                        break;
+                    case 6:
+                        
+                        break;
+                    case 0:
+                        exit = true; break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+
+                if (!exit)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
         }
     }
 }

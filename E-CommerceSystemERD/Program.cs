47ede8f4740fb5ded61435a6334e73ccc570279c
asilaalmaawali <asilaalmaawali123@gmail.com>
2026-07-09 09:12:13 +0400

    remove comment

--- E-CommerceSystemERD/Program.cs
+++ E-CommerceSystemERD/Program.cs
@@ -654,17 +654,6 @@ namespace E_CommerceSystemERD
                 Console.WriteLine("========================================");
                 Console.Write("Select option: ");
 
-
-
-
-                /*
-                 * case 10: //10 Get Category with All Its Products (Include)
-                        GetCategoryWithAllProducts();
-                        break;
-                    case 11: //11 View Order History with Full Details
-                        ViewOrderHistory();
-                        break;
-                 */
                 int option = int.Parse(Console.ReadLine());
 
                 switch (option)
